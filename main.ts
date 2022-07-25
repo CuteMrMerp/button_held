c098a0144327dbff0998a16292db565afbd27328
CuteMrMerp <106356937+CuteMrMerp@users.noreply.github.com>
2022-07-25 18:04:59 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-
+basic.showLeds(`
+    # # # # #
+    # . . . #
+    # # # # #
+    # . . . #
+    # . . . #
+    `)
+basic.forever(function () {
+    if (IsButtonHeld.held(1000, Button.A)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+    } else {
+    	
+    }
+})
