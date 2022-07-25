1a2a6fc94a8413aed78adfbf95eeb0cdf3e094d5
CuteMrMerp <106356937+CuteMrMerp@users.noreply.github.com>
2022-07-25 11:58:39 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
 basic.forever(function () {
-	
+    ButtonHeld.held(1000, Button.A, function () {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+    })
 })
